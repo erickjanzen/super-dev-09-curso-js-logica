89270a7cc586ed1fb897f9bc753bbbbdec43714c
Erick Janzen <erickjanzen554@gmail.com>
2026-04-17 11:17:15 -0300

    adicionar tarfea js atualzida

--- Atividade/assets/js/logica-04-mt-string-atividade.js
+++ Atividade/assets/js/logica-04-mt-string-atividade.js
@@ -23,12 +23,67 @@ function mostrarPrimeiraLetra(){
     let palavra = prompt("Digite uma palavra")
     let primLetra = palavra[0]
 
-    alert(primLetra)
+    if(palavra){
+        alert(primLetra)
+    }
 }
 
 function mostrarUltimaLetra(){
     let palavra = prompt("Digite uma palavra")
-    let ultLetra = palavra.lastIndexOf()
+    let ultLetra = palavra.length - 1
 
-    alert(1 + ultLetra[0])
+    if(palavra){
+        alert(palavra[ultLetra])
+    }
+}
+
+function verificarPalavraNaFrase(){
+    let frase = prompt("Digite uma frase");
+    let palavra = prompt("Digite uma palavra que está na frase");
+
+    let fraseM = frase.toLowerCase()
+    let palavraM = palavra.toLowerCase()
+
+    if ((fraseM)&&(palavraM)&&(fraseM.includes(palavraM)===true)) {
+        alert("A palavra que está na frase é: " + palavra)
+    } else {
+        alert("Essa palavra não está na frase")
+    }
+}
+
+function substituirCorNaFrase(){
+    let frase = prompt("Digite algum objeto e a cor azul")
+
+    if (frase.toLowerCase().includes("azul") === true){
+        alert(frase.replaceAll("azul", "vermelho"))
+    }
+}
+
+function trocarEstacao(){
+    let frase = prompt("Digite alguma frase com primavera")
+
+    if (frase.toLowerCase().includes("primavera") === true){
+        alert(frase.replaceAll("primavera", "verão"))
+    }
+}
+
+function primeirosCaracteres(){
+    let palavra = prompt("Digite uma palavra")
+
+    if(palavra.length >= 4){
+        alert(palavra.slice(0, 4))  
+    }
+}
+
+function separarNome(){
+    let nomeCompleto = prompt("Digite sem nome completo")
+    let nome = nomeCompleto.split(" ")
+    debugger
+    if(nome ){
+        alert("Nome: " + nome[0] + "\nSobrenome: " + nome[1] + "\nSobrenome: " + nome[2])
+    }
+}
+
+function removerEspacosExtras(){
+    
 }
